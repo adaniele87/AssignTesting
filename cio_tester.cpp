8206da7e4a7f0cde9fecb3f0be3e7b3f3f1f3583
adaniele87 <andrew_daniele@hotmail.com>
2013-09-28 00:17:37 -0400

    fixed case bug, adjusted length of phone array

--- cio_tester.cpp
+++ cio_tester.cpp
@@ -6,13 +6,13 @@ int main()
 {
     char author[19] = "by: Andrew Daniele";
     char name    [20] = "";
-    char phone   [9]  = ""; 
+    char phone   [15]  = ""; 
     char address [20] = "";
     int  position     = 0;
     int  key          = 0;
 
     console.display("Small Tester for Assignment 1", 0, 0);
-    console.display(author, 0, con.getCols()-1-bio::strlen(author));
+    console.display(author, 0, console.getCols()-1-bio::strlen(author));
 
     console.display("Name:    ", 2, 2);
     console.display("Phone:   ", 4, 2);
@@ -28,13 +28,13 @@ int main()
         switch (position)
         {
         case 0:
-            key = con.edit(name, 2, 11, 10, 19);
+            key = console.edit(name, 2, 11, 10, 19);
             break;
         case 1:
-            key = con.edit(phone, 4, 11, 10, 8);
+            key = console.edit(phone, 4, 11, 10, 14);
             break;
         case 2:
-            key = con.edit(address, 6, 11, 10, 19);
+            key = console.edit(address, 6, 11, 10, 19);
             break;
         }
 
@@ -47,10 +47,17 @@ int main()
         case DOWN:
             if (position < 2)
                 position++;
+            break;
         case ESCAPE:
-            !running;
+            running = false;
+            break;
         }
     }
 
+    console.display(name, 2, 11, 10);
+    console.display(phone, 4, 11, 10);
+    console.display(address, 6, 11, 10);
+    console.pause();
+    
     return 0;
 }
